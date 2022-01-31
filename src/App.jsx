da3aedee64777c1d8a2134b1c270170098113ded
MariemElG <mariem.el-gargouri@etu.umontpellier.fr>
2022-01-31 13:51:59 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Mariem + React!</p>
+        <p>Hello  + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
