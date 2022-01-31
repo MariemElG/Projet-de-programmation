a48051e51977a615c5aeea1c59d7385a9256fbb4
MariemElG <mariem.el-gargouri@etu.umontpellier.fr>
2022-01-31 16:20:30 +0100

    premiers pas creation de popUp

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,35 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import popUp from './Components/popUp';
 import Button from "react-bootstrap/Button";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  //const htmlelement = document.getElementById('#gam') : js
+ // htmlelement.innerHTML += "<div class=”card”><img src=”img/’ + pics[i] + ‘ “></div>" : js
+  // $(“#gameboard”).append(‘<div class=”card”><img src=”img/’ + pics[i] + ‘ “></div>’) : jquery
+
+  
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>qs npoo+ React!</p>
-        <p>Holla amigos + React!</p>
+       
+        <p>Important question : </p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 12)}>
-            count is: {count}
+          <button type="button"// onClick={() => setCount((count) => count + 12)}
+          >
+            <popUp trigger={}>
+              <h3>
+                Qui est-ce?
+              </h3>
+            </popUp>
+            Want to play ? 
           </button>
         </p>
         <p>
---
+++ src/Components/popUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { propTypes } from 'react-bootstrap/esm/Image'
+function popUp (props){
+    return (props.trigger) ? (
+        <div className = "popUp">
+             <div className = "popUp-inner">
+                 <button className="closebutton"> 
+                 Get rid of me
+                 </button>
+                 {propTypes.children}
+             </div>
+    
+
+        </div>
+    ) : "";
+}
+export default popUp
