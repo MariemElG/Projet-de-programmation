56e2d7c41887f641c6592588928b6e9c9bf4231e
Gün <74139498+gunwd@users.noreply.github.com>
2022-02-14 16:28:27 +0100

    progress!

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
+const random = () => Math.floor(Math.random() * data.possibilites.length); // choose random personnage
 
 function Personnage(props) {
   return (
@@ -20,17 +24,97 @@ function Personnage(props) {
   );
 }
 
+function Menu(props) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    selectAttribut("genre");
+  }
+
+  function selectAttribut(atr) {
+    const attributs = [];
+
+    data.possibilites.forEach((p) => {
+      if (!attributs.includes(p[atr])) attributs.push(p[atr]);
+    });
+
+    console.log(attributs);
+    return attributs;
+  }
+
+  return (
+    <form className="Menu" onSubmit={handleSubmit}>
+      <Button as="" variant="light">
+        Ajouter
+      </Button>{" "}
+      <Button variant="light">Enlever</Button>{" "}
+      <div>
+        <Dropdown onSelect={() => {}}>
+          <Dropdown.Toggle variant="light" id="dropdown-basic">
+            Attribut 1
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {Object.keys(data.possibilites[0]).map((key) => {
+              if (key != "fichier") {
+                return <Dropdown.Item eventKey={key}>{key}</Dropdown.Item>;
+              }
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+        <Dropdown>
+          <Dropdown.Toggle variant="light" id="dropdown-basic">
+            Attribut 2
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {Object.keys(data.possibilites[0]).map((key) => {
+              if (key != "fichier") {
+                return (
+                  <Dropdown.Item
+                    eventKey={Object.keys(data.possibilites[0]).indexOf(key)}
+                  >
+                    {key}
+                  </Dropdown.Item>
+                );
+              }
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <Button variant="light">Et</Button> <Button variant="light">Ou</Button>{" "}
+      <Button type="submit" variant="light">
+        Valider
+      </Button>{" "}
+    </form>
+  );
+}
+
 function Board(props) {
-  const [chosenPerson, setChosenPerson] = useState(null);
+  const chosenPerson = random();
+  // const [chosenPerson, setChosenPerson] = useState(random());
+  const [personnageElimine, setPersonnageElimine] = useState([]);
 
   return (
     <div>
-      {data.possibilites.map((p) => (
-        <Personnage
-          name={p.prenom}
-          src={"/" + data.locationImages + p.fichier}
-        />
-      ))}
+      {data.possibilites.map((p) => {
+        if (chosenPerson === data.possibilites.indexOf(p))
+          return (
+            <span className="ChosenPersonnage">
+              <Personnage
+                name={p.prenom}
+                src={"/" + data.locationImages + p.fichier}
+              />
+            </span>
+          );
+
+        return (
+          <Personnage
+            name={p.prenom}
+            src={"/" + data.locationImages + p.fichier}
+          />
+        );
+      })}
+      <hr></hr>
     </div>
   );
 }
@@ -39,6 +123,8 @@ function App() {
   return (
     <div className="App">
       <Board />
+
+      <Menu />
     </div>
   );
 }
