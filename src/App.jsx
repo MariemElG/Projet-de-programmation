992d7a8cd48ffecdb32562ff980330e65daf98a7
Gün <74139498+gunwd@users.noreply.github.com>
2022-02-21 19:00:25 +0100

    sync

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import Button from "react-bootstrap/Button";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import Form from "react-bootstrap/Form";
 
 const random = () => Math.floor(Math.random() * data.possibilites.length); // choose random personnage
@@ -26,46 +24,71 @@ function Personnage(props) {
 }
 
 function Menu(props) {
-  // TODO
-  function handleSubmit(event) {
-    event.preventDefault();
-    selectAttribut("genre");
-  }
-
-
   function AttributeSelector(props) {
-    function QualitySelector(props) {
-      const [showQualities, setShowQualities] = useState(false)
-      const onClick = () => setShowResults(true)
+    const [attribut, setAttribut] = useState(null);
+
+    function handleChange(e) {
+      console.log(e.target.value);
+      setAttribut(e.target.value);
     }
 
     return (
-      <Form.Select aria-label="Default select example" onSelect={() => { }}>
-        <option>Attribut</option>
-
-        {Object.keys(data.possibilites[0]).map((key) => {
-          if (key != "fichier") {
-            return <option value={key}>{key}</option>
-          }
-        })}
-      </Form.Select>
+      <div>
+        <Form.Select name="attribut" onChange={(e) => handleChange(e)}>
+          <option>Attribut</option>
+          {Object.keys(data.possibilites[0]).map((key) => {
+            if (key != "fichier") {
+              return <option value={key}>{key}</option>
+            }
+          })}
+        </Form.Select>
+        <QualitySelector attribut={attribut} />
+      </div>
     )
+
+    function QualitySelector(props) {
+      const filterQualities = (attribut) => {
+        const qualites = [];
+
+        data.possibilites.forEach((personnage) => {
+          if (!qualites.includes(personnage[attribut])) {
+            qualites.push(personnage[attribut])
+          }
+        })
+
+        return qualites;
+      }
+
+      if (!props.attribut) return (
+        <Form.Select name="qualite">
+          <option>Qualités</option>
+        </Form.Select>
+      )
+
+      return (
+        <Form.Select name="qualite">
+          {filterQualities(props.attribut).map((q) => (
+            <option value={q}>{q}</option>
+          ))}
+        </Form.Select>
+      )
+    }
   }
 
-
-
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (event.target.attribut.value === "Attribut") return alert("Choisir un attribut")
+    console.log(event.target.attribut.value)
+  }
 
   return (
-    <form className="Menu" onSubmit={handleSubmit}>
+    <form className="Menu" onSubmit={(event) => handleSubmit(event)}>
       <Button variant="light" onClick={() => { }}>
         Ajouter
       </Button>{" "}
       <Button variant="light" onClick={() => { }}>Enlever</Button>{" "}
       <div>
         <AttributeSelector />
-        <AttributeSelector />
-
       </div>
       <Button variant="light">Et</Button> <Button variant="light">Ou</Button>{" "}
       <Button type="submit" variant="light">
@@ -77,8 +100,6 @@ function Menu(props) {
 
 function Board(props) {
   const chosenPerson = random();
-  // const [chosenPerson, setChosenPerson] = useState(random());
-  const [personnageElimine, setPersonnageElimine] = useState([]);
 
   return (
     <div>
